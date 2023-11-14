04ef3462ea8dadfa672b3771a703395564a68ce5
valeriaalfaroo <valfaro2409@gmail.com>
2023-11-13 20:54:21 -0600

    login sin css

--- proyectofinal/src/components/login/login.js
+++ proyectofinal/src/components/login/login.js
@@ -2,19 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './login.module.css';
 import TextField from '@mui/material/TextField';
-
+import Button from '@mui/material/Button';
 
 const Login = () => (
   <div className={styles.Login} data-testid="Login">
-
-
-          <TextField id="standard-basic" label="Standard" variant="standard" />
+<h1> Bienvenido al Blog de Notas</h1>
+<form>
+<TextField id="standard-basic" label="Usuario" variant="standard" name="usuario" type="text"/>
+<br></br>
+<TextField id="standard-basic" label="Password" variant="standard" name="password" type="password" />
+<br></br>
+<br></br>
+<Button variant="contained" name="btnIngresar" type="submit">Ingresar</Button>
+<Button variant="contained" name="btnCancelar" type="reset">Cancelar</Button>
+</form>
+   
    <br></br>
-    Login Component (test1 Vale) 
-    Login Component (test2 Eduardo) 
-    Login Component (test3 Nacho)
-    Login Component (test4 Fabian) 
-
+   
 
   </div>
 );
